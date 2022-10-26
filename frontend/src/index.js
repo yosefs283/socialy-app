063a98cd7b4bfb70ce9c1ab864dd0669dc15b4cd
Yosef Semionov <yosefs283@gmail.com>
2022-10-26 18:03:56 +0300

    installed react and tailwindcss

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import  ReactDOM  from 'react-dom'
+import { createRoot } from 'react-dom/client';
+import App from './App'
+import './index.css'
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// ReactDOM.render(<App/>,document.getElementById('root'))
+root.render(<App />);
